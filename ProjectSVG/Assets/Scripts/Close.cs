b40eb0fb23fa589ab2be455f4a8bef8b5e7375dd
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2020-03-08 00:45:42 -0500

    Initial Commit

---
+++ ProjectSVG/Assets/Scripts/Close.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close : MonoBehaviour
+{
+    public bool Closed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Closed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape)) { Closed = !Closed; }
+        if (Closed) { Debug.Log("Exception Negative"); }
+    }
+}
---
+++ ProjectSVG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed;
+    private Rigidbody2D MoonPiss;
+    private Vector3 PissColorChange;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        MoonPiss = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PissColorChange = Vector3.zero;
+        PissColorChange.x = Input.GetAxis("Horizontal");
+        PissColorChange.y = Input.GetAxis("Vertical");
+
+        if (PissColorChange != Vector3.zero){
+            hahahaONE();
+            animator.SetFloat("moveX", PissColorChange.x);
+            animator.SetFloat("moveY", PissColorChange.y);
+        }
+    }
+    void hahahaONE()
+    {
+        MoonPiss.MovePosition(
+            transform.position + PissColorChange * Speed * Time.deltaTime
+            );
+    }
+}
